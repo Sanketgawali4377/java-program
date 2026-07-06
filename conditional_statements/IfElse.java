53e73e141b4b46c1b7ae079a949fe492613ff71d
sanket gawali <sanketgawali4377@gmali.com>
2026-07-07 01:09:27 +0530

    new file

--- conditional_statements/IfElse.java
+++ conditional_statements/IfElse.java
@@ -13,6 +13,7 @@ public class IfElse{
         else{
             System.out.println("Number is odd");
         }
+        
     }
-    
+   
 }
---
+++ conditional_statements/switchcase.java
@@ -0,0 +1,6 @@
+package conditional_statements;
+import java.util.*;
+public class swhichcase {
+    Scanner sc = new Scanner(System.in);
+    int num = sc.nextInt();
+}
