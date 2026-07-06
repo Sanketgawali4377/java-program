d162ecf2f7fe1dd627df9fbf9c9bf3bd29808e4f
sanket gawali <sanketgawali4377@gmali.com>
2026-07-07 00:33:56 +0530

    new file

---
+++ conditional_statements/Elseif.java
@@ -0,0 +1,26 @@
+package conditional_statements;
+
+import java.util.*;
+
+public class Elseif {
+   public static void main(String args[]){ 
+    Scanner sc = new Scanner(System.in);
+    System.out.print("enter a");
+    int a = sc.nextInt();
+    System.out.print("enter b");
+    int b = sc.nextInt();
+    if (a==b){  
+        System.out.println("a is equal to b");
+    }
+    else{
+        if(a>b){
+            System.out.println("a is bigger than b");
+        }
+    else{
+        if(a<b){
+            System.out.println("a is smaller than b");
+        }
+    }
+}
+}
+}
---
+++ conditional_statements/IfElse.java
@@ -0,0 +1,18 @@
+package conditional_statements;
+import java.util.*;
+
+public class IfElse{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter a Number :");
+        int number = sc.nextInt();
+        if(number % 2 == 0){
+            System.out.println("Number is even");
+        }
+        else{
+            System.out.println("Number is odd");
+        }
+    }
+    
+}
