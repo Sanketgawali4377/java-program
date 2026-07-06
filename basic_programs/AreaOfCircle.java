deb58c61e82d8c98c9fb7a2391fc6016190d2e60
sanket gawali <sanketgawali4377@gmali.com>
2026-07-06 20:30:40 +0530

    area of circle program

---
+++ basic_programs/AreaOfCircle.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class student {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       System.out.print("enter redius :");
+       int radius = sc.nextInt();
+       double area = 3.14 * radius * radius;
+       System.out.println("area =" +area);
+       sc.close() ;
+    }
+}
