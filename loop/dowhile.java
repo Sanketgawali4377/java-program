2b08d5655bf92fc1fc2f7967000955a94fdc94d9
sanket gawali <sanketgawali4377@gmail.com>
2026-07-08 15:17:35 +0530

    do while program

--- loop/dowhile.java
+++ loop/dowhile.java
@@ -1,6 +1,39 @@
 package loop;
 
+import java.util.Scanner;
+
 public class dowhile {
-    
-    
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int choise;
+
+        do {
+
+            System.out.println("Enter Number From 0 To 1:");
+            choise = sc.nextInt();
+
+            if (choise == 1) {
+
+                System.out.println("Enter Marks From 0 To 100:");
+                int marks = sc.nextInt();
+
+                if (marks >= 90) {
+                    System.out.println("Good Marks");
+                } else if (marks >= 60 && marks <= 89) {
+                    System.out.println("Also Good Marks");
+                } else if (marks >= 0 && marks <= 59) {
+                    System.out.println("This is Good as well because marks don't matter but our effort does.");
+                } else {
+                    System.out.println("Invalid Marks");
+                }
+            }
+
+        } while (choise != 0);
+
+        System.out.println("Program is ended");
+        sc.close();
+    }
 }
--- loop/forloop.java
+++ loop/forloop.java
@@ -3,11 +3,12 @@ import java.util.*;
 public class forloop {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter the number");
+        System.out.print("Enter The Number");
         int n = sc.nextInt();
-        for(int i=2;i<=n;i=i+2){
+        for(int i=2 ; i<=n ; i=i+2){
             System.out.println(i);
         }
+        sc.close();
         }
     }
 
