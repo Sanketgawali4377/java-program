db1fddfb69ca551809012a3f292135417f2a7065
sanket gawali <sanketgawali4377@gmail.com>
2026-07-09 22:44:27 +0530

    inverted pyramid

--- patterns/inverted_half_pyramid.java
+++ patterns/inverted_half_pyramid.java
@@ -1,14 +1,15 @@
 package patterns;
-public class halfpyramid{
+
+public class inverted_half_pyramid {
     public static void main(String[] args){
-        int n = 4;
+        int n=4;
         for(int i=n; i>=1; i--){
             for(int j=1; j<=i; j++){
-               
                     System.out.print("*");
-                
+                }
+        
+             System.out.println();
             }
-            System.out.println();
+           
         }
-    }
 }
