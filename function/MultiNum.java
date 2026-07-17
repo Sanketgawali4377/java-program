1c0ed7ad1800ab356eddc9797327e92ceb47e794
sanket gawali <sanketgawali4377@gmail.com>
2026-07-17 19:04:22 +0530

    factorial program

---
+++ function/MultiNum.java
@@ -0,0 +1,20 @@
+package function;
+
+import java.util.Scanner;
+
+public class MultiNum {
+    public static int calculate(int a, int b){
+
+        return a*b;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter The First Value :");
+            int a = sc.nextInt();
+        System.out.print("Enter The Second Value :");  
+            int b = sc.nextInt();
+        
+    System.out.println("multiply of two number :" + calculate( a, b));
+    
+    }
+}
