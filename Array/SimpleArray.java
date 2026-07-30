f45f297b8afedb0d956ea5883fc0aaf59a6706d2
sanket gawali <sanketgawali4377@gmail.com>
2026-07-30 11:11:37 +0530

    arrays folder

---
+++ Array/SimpleArray.java
@@ -0,0 +1,22 @@
+package Array;
+
+import java.util.Scanner;
+
+public class SimpleArray {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number :");
+        int size = sc.nextInt();
+
+
+        int arr[] = new int[size];
+        
+        for(int i=0 ; i<size; i++){
+            System.out.print(arr[i]);
+        }
+
+        
+        
+    }
+    
+}
