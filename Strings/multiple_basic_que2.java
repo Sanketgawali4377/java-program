ef1067955bfcd22fa60628af213214a2dc8cde7b
sanket gawali <sanketgawali4377@gmail.com>
2026-08-04 00:21:47 +0530

    multiple quesion practice

---
+++ Strings/multiple_basic_que2.java
@@ -0,0 +1,47 @@
+/*Find the first and last character of a string.
+Reverse a string without using StringBuilder.reverse().
+Check whether a string is empty.
+Print all characters at even indices.
+Print all characters at odd indices.*/
+
+
+package Strings;
+
+import java.util.Scanner;
+
+public class multiple_basic_que2 {
+    public static void main(String[] args){
+
+    //Find the first and last character of a string
+
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter String :");
+    String name = sc.nextLine();
+
+    //Find the first and last character of a string.
+        System.out.print("first letter :");
+        System.out.println(name.charAt(0));
+
+
+       
+        int a = name.length() - 1;
+        System.out.println("last letter :" + name.charAt(a));
+
+    //Check whether a string is empty.
+    
+   if(name.isEmpty()){
+    System.out.println("String Is Empty :");
+   }else{
+    System.out.println("String Not Empty :");
+   }
+
+    
+    sc.close();
+}
+    
+}
+
+
+
+
+
