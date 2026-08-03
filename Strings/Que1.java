771d4098bdb9a57149a44cf900408330e0fd9e50
sanket gawali <sanketgawali4377@gmail.com>
2026-08-03 13:44:54 +0530

    Que 3

--- Strings/Que1.java
+++ Strings/Que1.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class Que1 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size : ");
         int size = sc.nextInt();
 
         String[] arr = new String[size];
