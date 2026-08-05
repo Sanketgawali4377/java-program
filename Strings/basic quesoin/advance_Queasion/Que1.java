e95a7f3e766b76422e4da73f2e2ce5e639baf75c
sanket gawali <sanketgawali4377@gmail.com>
2026-08-05 17:36:37 +0530

    multiple queasion medium difficulty

--- Strings/basic quesoin/advance_Queasion/Que1.java
+++ Strings/basic quesoin/advance_Queasion/Que1.java
@@ -1,9 +1,11 @@
-/*Compare two strings using compareTo().
-Check whether two strings are equal using equals().
+/*Extract the first 5 characters using substring().
+Extract the last 4 characters using substring().
 Print the first half of a string.
 Print the second half of a string.
-Extract the first 5 characters using substring().
-Extract the last 4 characters using substring().
+Compare two strings using compareTo().
+Check whether two strings are equal using equals().
+
+
 Count how many times a particular character appears.
 Find the index of the first occurrence of a character (without indexOf()).
 Find the index of the last occurrence of a character (without lastIndexOf()).
@@ -13,42 +15,66 @@ package advance_Queasion;
 import java.util.*;
 
 
-public class Que1{
-    public static void main(String[] args){
-        //Compare two strings using compareTo().
+public class Que1 {
+    public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter Name :");
+        System.out.print("Enter Name 1: ");
         String name1 = sc.nextLine();
-        System.out.println("Enter Name :");
+
+        System.out.print("Enter Name 2: ");
         String name2 = sc.nextLine();
 
+        // Compare two strings
         if (name1.compareTo(name2) == 0) {
             System.out.println("Strings are equal");
-        }else{
-            System.out.println("String Are Not Equal");
+        } else {
+            System.out.println("Strings are not equal");
         }
 
+        // First half
+        String firstHalf = name1.substring(0, name1.length() / 2);
+        System.out.println("First Half: " + firstHalf);
 
-//Print the first half of a string.
-//Print the second half of a string.
+        // Second half
+        String secondHalf = name1.substring(name1.length() / 2);
+        System.out.println("Second Half: " + secondHalf);
 
-        for(int i=0; i<name1.length()/2; i++){
-            System.out.println("First Half :" + name1.substring(0, name1.length() / 2)
-            );
+
+
+        //Extract the first 5 characters using substring().
+        //Extract the last 4 characters using substring().
+
+        System.out.println("First Five Charecter Of First String: "+ name1.substring(0,5));
+        
+        System.out.println("First Four Charecter Of First String: " + name1.substring(0,4));
+        
+        System.out.println("First Five Charecter Of First String: " + name2.substring(0,5));
+
+        System.out.println("First Four Charecter Of First String: " + name2.substring(0,4));
+
+
+        //Check whether two strings are equal using equals().
+       if(name1.equals(name2)){
+        System.out.println("String Are Equal: ");
+       }else{
+        System.out.println("String Are Not Equal");
+       }
+
+        //Count how many times a particular character appears.
+       System.out.print("Enter The Element: ");
+        char a = sc.next().charAt(0);
+
+        int count =0;
+        for(int i=0; i<name1.length(); i++){
+            if(a==name1.charAt(i)){
+                count++;
+            }
             
         }
-        for(int i=0; i<name1.length()/2; i++){
-            System.out.println("Second Half :" + name1.substring(name1.length() / 2 , name1.length()));
-            
-            
-        }
-        }
-
-       
-
+            System.out.println("count :"+ count);
+        
+        sc.close();
     }
-
-
-
-
+}
